0a8b258747be89c2521416accdeb17ff73c95afb
WTCHAI <103483111+WTCHAI@users.noreply.github.com>
2024-07-05 23:22:58 +0700

    Adding missing packgae

--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -1,8 +1,15 @@
 const hre = require("hardhat");
 
-async function deployHello(){
-    const Hello = await hre.ethers.getContractFactory("HelloWorld0");
-    const hello = await Hello.deploy();
-    await hello.deployed();
-    console.log("Hello deployed to:", hello.address);
+async function main() {
+  const Hello = await hre.ethers.getContractFactory("HelloWorld");
+  const hello = await Hello.deploy();
+  await hello.deployed();
+  console.log("Hello deployed to:",);
 }
+
+main()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
+  });
