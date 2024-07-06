49b9b6b56adf62bfad0bbb90939e486a202a1b74
WTCHAI <103483111+WTCHAI@users.noreply.github.com>
2024-07-06 16:18:18 +0700

    Remove all files

--- src/app/page.tsx
+++
@@ -1,9 +0,0 @@
-import Image from "next/image";
-
-export default function Home() {
-  return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h1>Lorem ipsum dolor sit amet consectetur adipisicing elit. Deserunt natus quaerat sunt quasi iure! Nulla minus natus beatae a molestiae doloribus pariatur quam vero, ad illo tenetur fuga, facilis impedit.</h1>
-    </main>
-  );
-}
