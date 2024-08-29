20b16bddd370f66b9a5a6b3532096b4fa7eca130
WTCHAI <103483111+WTCHAI@users.noreply.github.com>
2024-08-29 21:29:46 +0700

    Network configuration sepolia & init zksync

--- hardhat/hardhat.config.ts
+++ hardhat/hardhat.config.ts
@@ -1,8 +1,22 @@
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
+import "@matterlabs/hardhat-zksync-deploy";
+
 
 const config: HardhatUserConfig = {
   solidity: "0.8.24",
+  networks: {
+    sepolia: {
+      url: `https://eth-sepolia.g.alchemy.com/v2/${process.env.ALCHEMY_API_KEY}`,
+      accounts: [`${process.env.PRIVATE_KEY_MY_WALET_SEPOLIA}`],
+    },
+    zkTestnet: {
+      url: "https://sepolia.era.zksync.dev", // The testnet RPC URL of ZKsync Era network.
+      ethNetwork: "sepolia", // The Ethereum Web3 RPC URL, or the identifier of the network (e.g. `mainnet` or `sepolia`)
+      zksync: true
+    }
+
+  }
 };
 
 export default config;
