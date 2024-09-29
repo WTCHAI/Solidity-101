06dfccc8c643ad03d7054e61a7d08f32b9a94033
WTCHAI <103483111+WTCHAI@users.noreply.github.com>
2024-09-29 12:21:48 +0700

    Connecting wallet handlering in Hook & frontend migration

--- Solidity_example/src/custom-hooks/useConnectWallet.ts
+++ Solidity_example/src/custom-hooks/useConnectWallet.ts
@@ -1,22 +1,34 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 import { ethers } from "ethers";
- 
+
+import { UserInfoInterface } from "../interface/ConnectWallet/InterfaceUserInfo";
 
 export const useConnectWallet = () =>{
     const [connected,SetConnection] = useState(false)
-    const [address,SetSigner] = useState('null')
-    // const [provider ]
+    const [ userInfo , SetUserInfo] = useState({} as UserInfoInterface)
+
     const OnConnectToWallet = async ()=>{
         if (!connected){
             try { 
+                console.log("Connect clicked ")
                 const provider = new ethers.BrowserProvider(window.ethereum)
-                const signers = await provider.getSigner();
-                console.log('provider : ',provider._network)
-                console.log('signers  : ',signers)
-                console.log()
-                SetSigner(await signers.getAddress())
+                const signer = await provider.getSigner();
+                const accounts = await provider.send("eth_requestAccounts",[])
+
+                SetUserInfo({
+                    ...userInfo,
+                    walletAddress: await signer.getAddress(),
+                    accounts: accounts,
+                    provider : provider,
+                    signers : signer
+                })
                 SetConnection(true)
+
+                
+                window.ethereum.on("accountsChanged",handleAccountsChanged)
+                window.ethereum.on("chainChanged",handleChainChanged)
+
             }catch(error){
                 SetConnection(false)
             }
@@ -24,12 +36,76 @@ export const useConnectWallet = () =>{
     }
 
     const OnDisconnectWallet = async ()=>{
-        SetConnection(false)
+        SetConnection(false);
+        SetUserInfo({
+            ...userInfo,
+            walletAddress:undefined,
+            accounts: undefined,
+            signers: undefined,
+            provider: undefined,
+        })
+        
+        if (window.ethereum) {
+            window.ethereum.removeListener("accountsChanged", handleAccountsChanged);
+            window.ethereum.removeListener("chainChanged", handleChainChanged);
+        }
     }
+
+    const handleAccountsChanged = async (accounts : any ) => {
+        try{
+            if(accounts.length > 0){
+                // user change account to new current which keep in account[0]
+                // console.log("Change account to ",accounts[0])
+
+                // considering using this practice chaging account doesn't get new provider and signers which might address unbehavior when compare walletAddress & signers.getAddress so considering using below approch 
+                // SetUserInfo({
+                //     ...userInfo,
+                //     walletAddress: accounts[0],
+                //     accounts: accounts
+                // })
+                
+                const provider = new ethers.BrowserProvider(window.ethereum)
+                const signer = await provider.getSigner();
+                const curWalletAddress  = await signer.getAddress();
+                SetUserInfo({
+                    ...userInfo,
+                    provider: provider,
+                    walletAddress: curWalletAddress,
+                    accounts: accounts,
+                    signers: signer
+                });                     
+                
+            }else {
+                OnDisconnectWallet()
+            }
+        }catch(err){
+            throw new Error("An error occurred while checking wallet change");
+        }
+    }
+
+    const handleChainChanged = (chainId : any)=>{
+        try{
+            console.log("Network change to :",chainId)
+            window.location.reload();
+        }catch(err){
+            throw new Error("An error occurred while checking wallet change");
+        }
+    }
+
+    useEffect(()=>{
+        return () => {
+            // Clean up event listeners when the component unmounts
+            if (window.ethereum) {
+                window.ethereum.removeListener("accountsChanged", handleAccountsChanged);
+                window.ethereum.removeListener("chainChanged", handleChainChanged);
+            }
+        };
+    },[])
 
     return {
         OnConnectToWallet,
         OnDisconnectWallet,
-        address
+        userInfo : userInfo,
+
     }
 }
--- Solidity_example/src/pages/connect-wallet.tsx
+++ Solidity_example/src/pages/connect-wallet.tsx
@@ -1,22 +1,37 @@
+import { useEffect } from "react"
 import { useConnectWallet } from "../custom-hooks/useConnectWallet"
 
 export default function ConnectWallet() {
-    const { address , OnConnectToWallet ,OnDisconnectWallet } = useConnectWallet()
-    
+    const { userInfo , OnConnectToWallet ,OnDisconnectWallet } = useConnectWallet()    
+
+    useEffect(()=>{
+      // OnConnectToWallet()
+    },[window.ethereum])
     
     return (
       <div className="flex flex-col ">
 
           connect wallet metamask 
-        <h1>
-          Address : {address}
-        </h1>
         <button
-          className="p-5 bg-slate-300"
+          className="p-5 bg-green-400"
           onClick={OnConnectToWallet}
         >
           Connnect to wallet
         </button>
+        <button 
+          className="p-5 bg-red-300"
+          onClick={OnDisconnectWallet}
+        >
+          Disconnect wallet 
+        </button>
+        <div>
+              <h3>Connected Wallet Info</h3>
+              <p>Address: {userInfo.walletAddress}</p>
+              <p>
+                Chain Id : {userInfo.provider?._network.chainId.toLocaleString()}
+              </p>
+              <p>Provider: {userInfo.provider ? "Connected" : "Not Connected"}</p>
+          </div>
       </div>
     )
   }
